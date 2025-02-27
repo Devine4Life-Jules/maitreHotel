8cd87fa39af28d85c1a2b7ca060ae4bcadd8f3ab
JulesDevine <jules.desmet@student.howest.be>
2025-02-27 14:32:38 +0100

    Added Possibility to add a table

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,25 @@ import Table from './Table'
 import './App.css'
 
 function App() {
-  
+
+  const [tables, setTables] = useState([1]); 
+
+  const addTable = () => {
+    setTables([...tables, tables.length + 1]); 
+  };
 
   return (
-    <div className='tableContainer'>
-      <Table />
-      <Table />
+    <div className="overview">
+      <div className="tableContainer">
+        {/* Render tables based on the state */}
+        {tables.map((tableId) => (
+          <Table key={tableId} />
+        ))}
+      </div>
+      <button onClick={addTable}>Add Table</button>
     </div>
-    
+  );
 
-  )
 }
 
 export default App
