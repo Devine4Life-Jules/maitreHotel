884b54226e37d56357f5e10effc954aea7f79e58
JulesDevine <jules.desmet@student.howest.be>
2025-03-20 15:07:19 +0100

    got the floorplan rendered out

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import TryOut1 from './components/tryout1';
+import FloorPlan from './components/floorplan';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return(
     <div>
-      <TryOut1> </TryOut1>
+      <FloorPlan></FloorPlan>
       
 
     </div>
---
+++ src/components/FloorPlan.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+
+const FloorPlan = () => {
+    return (
+        <div style={{ height: "60vh", width: "60vw" }}>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1920 1080">
+                <g>
+                    <g id="Layer_1">
+                        <g>
+                            <rect fill="#635e5e" x=".5" y=".5" width="1919" height="1079" />
+                            <path d="M1919,1v1078H1V1h1918M1920,0H0v1080h1920V0h0Z" />
+                        </g>
+                    </g>
+                    <g id="Bar">
+                        <g>
+                            <rect fill="#b7b7b7" x="1079.1" y="133.1" width="840.4" height="134.2" />
+                            <path d="M1919,133.6v133.2h-839.4v-133.2h839.4M1920,132.6h-841.4v135.2h841.4v-135.2h0Z" />
+                        </g>
+                    </g>
+                    <g id="table1">
+                        <g>
+                            <rect fill="#b7b7b7" x="178.6" y="567.7" width="196.5" height="396.3" />
+                            <path d="M374.6,568.2v395.3h-195.5v-395.3h195.5M375.6,567.2h-197.5v397.3h197.5v-397.3h0Z" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="411.2" y="860.1" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="411.2" y="723.8" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="411.2" y="587.5" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="55" y="860.1" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="55" y="723.8" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="55" y="587.5" width="89.4" height="89.4" />
+                        </g>
+                    </g>
+                    <g id="table3">
+                        <g>
+                            <rect fill="#b7b7b7" x="1310.4" y="666.7" width="396.3" height="196.5" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1319.9" y="899.2" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1456.2" y="899.2" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1592.5" y="899.2" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1319.9" y="543.1" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1456.2" y="543.1" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1592.5" y="543.1" width="89.4" height="89.4" />
+                        </g>
+                    </g>
+                    <g id="table2">
+                        <g>
+                            <rect fill="#b7b7b7" x="822.6" y="679.7" width="196.5" height="222" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="698.6" y="814.2" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="698.6" y="677.9" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1045.5" y="814.2" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="1045.5" y="677.9" width="89.4" height="89.4" />
+                        </g>
+                    </g>
+                    <g id="table6">
+                        <g>
+                            <rect fill="#efefef" x="600.3" y="82.5" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="600.3" y="363.7" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#b7b7b7" x="566" y="186.3" width="158.1" height="158.1" />
+                        </g>
+                    </g>
+                    <g id="table5">
+                        <g>
+                            <rect fill="#efefef" x="376.8" y="82.5" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="376.8" y="363.7" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#b7b7b7" x="342.5" y="186.3" width="158.1" height="158.1" />
+                        </g>
+                    </g>
+                    <g id="table4">
+                        <g>
+                            <rect fill="#efefef" x="152.7" y="82.5" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#efefef" x="152.7" y="363.7" width="89.4" height="89.4" />
+                        </g>
+                        <g>
+                            <rect fill="#b7b7b7" x="118.3" y="186.3" width="158.1" height="158.1" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    );
+};
+
+export default FloorPlan;
