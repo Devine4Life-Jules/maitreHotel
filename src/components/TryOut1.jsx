ce17dcea37d7907f81fb9845405e5f3d147e5f09
JulesDevine <jules.desmet@student.howest.be>
2025-03-20 14:41:49 +0100

    added tryout 1 component that i can easily delete / re apply if my second try out doesnt end up being better than the firs

---
+++ src/components/TryOut1.jsx
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+import Area from './Area';
+
+
+const TryOut1 = () => {
+
+
+    const [areas, setAreas] = useState([{ id: 1 },
+        { id: 2 },]);
+      const addArea = () => {
+        setAreas([...areas, { id: Date.now() }]);  
+      };
+    
+    
+      const deleteArea = (id) => {
+        setAreas(areas.filter(area => area.id !== id));
+      };
+
+      
+        return (
+            <div>
+                <div className="areaContainer">
+                
+                <div>
+                {areas.map((area, index) => (
+                    <Area
+                    key={area.id}
+                    areaId={area.id}
+                    deleteArea={() => deleteArea(area.id)}
+                    />
+                ))}
+                </div>
+                        </div>
+                        <button onClick={addArea}>Add Area</button>
+            </div>
+        )
+    
+}
+
+export default TryOut1;
+
+
+
+
