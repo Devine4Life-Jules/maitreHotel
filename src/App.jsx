28daa59384a1e691f461d0ecf636faba12dffc71
JulesDevine <jules.desmet@student.howest.be>
2025-03-25 09:08:00 +0100

    removed tryout import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-import TryOut1 from './components/tryout1';
-import FloorPlan from './components/floorplan';
 import './App.css';
+import FloorPlan from './components/floorplan';
 
 function App() {
 //test
