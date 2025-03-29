6364eb99c1a37ac146e575c77086be0b68a1d168
JulesDevine <jules.desmet@student.howest.be>
2025-03-29 16:11:55 +0100

    added more products

--- src/availableProducts.js
+++ src/availableProducts.js
@@ -2,7 +2,14 @@ const availableProducts = [
     { name: "Spaghetti", price: 18 },
     { name: "Hamburger", price: 19 },
     { name: "Croque Monsieur", price: 14 },
+    { name: "Steak", price: 24 },
     { name: "Water", price: 2 },
+    { name: "Beer", price: 2 },
+    { name: "Wine Glass", price: 5 },
+    { name: "Wine Bottle", price: 20 },
+
+
+
 ];
 
 export default availableProducts;
