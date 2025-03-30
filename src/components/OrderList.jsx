a99cfad75fb7cbca08cc3dd636e857fd9bb2fb7a
JulesDevine <jules.desmet@student.howest.be>
2025-03-30 11:46:26 +0200

    css improvement to the ordering menu

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -4,9 +4,10 @@ const OrderList = ({ orders, onIncrease, onDecrease, onRemove }) => {
             {orders?.length > 0 ? (
                 orders.map((product, index) => (
                     <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <button onClick={() => onDecrease(product.name)}>-</button>
                         <p>{product.name} - ${product.price} (x{product.quantity})</p>
 
-                        <button onClick={() => onDecrease(product.name)}>-</button>
+                        
                         <button onClick={() => onIncrease(product)}>+</button>
                         <button onClick={() => onRemove(product.name)} style={{ backgroundColor: 'red', color: 'white' }}>
                             Remove
